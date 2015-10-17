b689d9c82979ac7403fbbf70d2f1bd6f15590ce9
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-17 10:11:51 -0600

    added index.php and started note process

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Website Project Initial Notes</title>
+	</head>
+	<body>
+		<h1>PWP Intital Notes and Brainstorming</h1>
+		<p></p>
+	</body>
+</html>
