5ecc8bfed5263473165eb4ee5344a25179e1213d
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-19 09:17:27 -0600

    added domain ideas and words for domain ideas

---
+++ domain-ideas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Domain Name Ideas</title>
+	</head>
+	<body>
+		<h3>Domain Name Idea</h3>
+		<p>Fitness Synonyms</p>
+		<ul>
+			<li>Fitness</li>
+			<li>Movement</li>
+			<li>Health</li>
+			<li>Wellness</li>
+			<li>Strength</li>
+			<li>Train</li>
+			<li>Mobility</li>
+			<li>Supple</li>
+			<li>Agile</li>
+			<li>Graceful</li>
+			<li>Flow</li>
+		</ul>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,23 @@
 	</head>
 	<body>
 		<h1>PWP Intital Notes and Brainstorming</h1>
-		<p></p>
+		<p>Logo idea - cat on a yoga mat or near a loaded barbell. Stylistically following Composer and The Pidegeon
+		Gazette. Cat coloration and yoga mat and or barbell change 4 to 5 times with each reload. see composer.com for
+		details.
+		<br>
+		Happiness through better movement. Learn to breath. Learn to move.
+		<br>
+		Move Excellently. Breathe Deeply.
+		<br>
+		Navigation - Home Services Schedule Contact About</p>
+		Footer - testimonials blog
+		<br>
+		Find a place to put recommendations
+		<ul>
+			<li>Blue Sky Chairopractic - Dr. Emily Blair</li>
+			<li>Aruyvedic Institute of NM</li>
+			<li>ICWA for Weightlifting Classes and Mobility</li>
+			<li>Acupuncturist?</li>
+		</ul>
 	</body>
 </html>
