d47125bfbfe1325d34bd5f6566d96155d522610f
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-27 13:13:15 -0600

    added tiny stylesheet

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -16,6 +16,8 @@
 		//// <link type="text/css" href="css/style.css" rel="stylesheet"/>
 		///////////////////////////////////////////////////// -->
 
+		<link type="text/css" href="stylesheet.css" rel="stylesheet"/>
+
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
@@ -41,4 +43,4 @@
 			<h1>Hello, world! I'm a Bootstrap page! :D</h1>
 		</div>
 	</body>
-</html>added b
+</html>
