6abe6502e8536a1b92a4fea306a5ae6763417cbe
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-29 16:11:49 -0600

    added nav code to test

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -17,6 +17,7 @@
 		///////////////////////////////////////////////////// -->
 
 		<link type="text/css" href="stylesheet.css" rel="stylesheet"/>
+		<link type="text/css" href="animate.css" rel="stylesheet"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,9 +40,30 @@
 		<title>Bootstrap 101 Template (DDC Version!)</title>
 	</head>
 	<body>
-		<div class="header">
-			<h1>LOTUS CAT</h1>
-			<h5>move well. breathe deep. lift heavy.</h5>
+		<div class="container">
+			<h1 class="animated fadeInDown">LOTUS CAT</h1>
+			<nav class="navbar navbar-inverse">
+				<!-- logo and mobile toggle button get grouped together for better mobile display -->
+				<div class="navbar-header">
+					<!-- this is the mobile menu button -->
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+						<span class="sr-only">main menu</span>
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+					</button>
+					<a class="navbar-brand" href="#">Your Logo/Text Here</a>
+				</div>
+
+				<!-- here are your main nav links, grouped for toggling -->
+				<div class="collapse navbar-collapse" id="main-menu">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Link1</a></li>
+						<li><a href="#">Link2</a></li>
+						<li><a href="#">Link3</a></li>
+					</ul>
+				</div>
+			</nav>
+
 		</div>
 		<div class="container">
 			<p>Look at this fabulous bootstrap page! Hooray for CSS!</p>
@@ -50,7 +72,7 @@
 			<div class="col-md-6">Second column</div>
 		</div>
 		<div class="row">
-			<div class="col-md-4">second hello</div>
+			<div class="col-md-4"><button>second hello</button></div>
 			<div class="col-md-4">third hello</div>
 			<div class="col-md-4">fourth hello</div>
 		</div>
