6beccbbb40849f52b76ba6f5cfb68b211b878a96
Kimberly Keller <kimberly@gravitaspublications.com>
2015-10-29 16:13:49 -0600

    changing up the nav

--- bootstrap-test.php
+++ bootstrap-test.php
@@ -40,17 +40,15 @@
 		<title>Bootstrap 101 Template (DDC Version!)</title>
 	</head>
 	<body>
-		<div class="container">
-			<h1 class="animated fadeInDown">LOTUS CAT</h1>
 			<nav class="navbar navbar-inverse">
 				<!-- logo and mobile toggle button get grouped together for better mobile display -->
 				<div class="navbar-header">
 					<!-- this is the mobile menu button -->
+					<h1 class="animated fadeInDown">LOTUS CAT</h1>
 					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
 						<span class="sr-only">main menu</span>
 						<span class="glyphicon glyphicon-menu-hamburger"></span>
 					</button>
-					<a class="navbar-brand" href="#">Your Logo/Text Here</a>
 				</div>
 
 				<!-- here are your main nav links, grouped for toggling -->
